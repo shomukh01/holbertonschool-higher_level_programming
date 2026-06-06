e56019fb6185e1010fd48a8c328850035927fe5b
Shomukh01 <Shomukh.aldosari@gmail.com>
2026-06-06 00:29:22 +0000

    Fix 8-concat_edges.py

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-word = "Holberton"
-print(word[3:] + word[:2] + word[5:] + " " + word[0] +
-      word[1:3] + word[4] + " " + word[:2] + word[5:])
+str = "Python is an interpreted, interactive, object-oriented programming \
+language that combines remarkable power with very clear syntax"
+print(str[39:66] + str[106:112] + str[:6])
