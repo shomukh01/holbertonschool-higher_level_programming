0fb2d198f378a4bf5f2fa66a734b30a5a106c6ca
Shomukh AlDosari <shomukh.aldosari@gmail.com>
2026-06-13 16:24:44 +0000

    Task 0: Integers addition

---
+++ python-exceptions/python-test_driven_development/0-add_integer.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""This module provides a function that adds two integers."""
+
+
+def add_integer(a, b=98):
+    """Return the sum of two integers.
+
+    Floats are cast to integers before addition.
+    """
+
+    if not isinstance(a, (int, float)):
+        raise TypeError("a must be an integer")
+
+    if not isinstance(b, (int, float)):
+        raise TypeError("b must be an integer")
+
+    return int(a) + int(b)
