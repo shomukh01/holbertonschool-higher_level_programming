8af51c2cfd852f3ab799e174dc3deaa223a6a01c
Shomukh AlDosari <shomukh.aldosari@gmail.com>
2026-06-13 20:03:23 +0000

    Fix NaN handling

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -11,7 +11,14 @@ def add_integer(a, b=98):
     if not isinstance(b, (int, float)):
         raise TypeError("b must be an integer")
 
-    a = int(a)
-    b = int(b)
+    try:
+        a = int(a)
+    except ValueError:
+        raise TypeError("a must be an integer")
+
+    try:
+        b = int(b)
+    except ValueError:
+        raise TypeError("b must be an integer")
 
     return a + b
