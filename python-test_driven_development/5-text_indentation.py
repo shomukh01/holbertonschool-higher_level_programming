48b143f4201e4485a4798ca3ec16b7f31ddd5734
Shomukh AlDosari <shomukh.aldosari@gmail.com>
2026-06-13 23:14:16 +0000

    Task 5 text indentation

---
+++ python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Module for text indentation."""
+
+
+def text_indentation(text):
+    """Print text with 2 new lines after ., ? and :."""
+
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+
+    i = 0
+
+    while i < len(text):
+        print(text[i], end="")
+
+        if text[i] in ".?:":
+            print("\n")
+            i += 1
+            while i < len(text) and text[i] == " ":
+                i += 1
+            continue
+
+        i += 1
