25a373e55169dcbe8f84e4b559f75444e1293111
shomukh01 <shomukh.aldosari@gmail.com>
2026-06-14 21:36:28 +0000

    Handle string operands

--- python-test_driven_development/101-lazy_matrix_mul.py
+++ python-test_driven_development/101-lazy_matrix_mul.py
@@ -7,7 +7,7 @@ import numpy as np
 def lazy_matrix_mul(m_a, m_b):
     """Multiply two matrices using NumPy."""
 
-    if type(m_a) is str:
+    if type(m_a) is str or type(m_b) is str:
         raise ValueError("Scalar operands are not allowed, use '*' instead")
 
     return np.dot(m_a, m_b)
