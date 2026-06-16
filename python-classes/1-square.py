b99cb8580a40c18a44686fe9b37ab40407cc0abb
Shomukh Al Dosari <shomukh.aldosari@gmail.com>
2026-06-16 10:48:27 +0000

    Task 1

---
+++ python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Defines a square."""
+
+
+class Square:
+    """Represents a square."""
+
+    def __init__(self, size):
+        """Initialize a square."""
+        self.__size = size
