7f0e53b8ab459ffb5cb94c5afc96d631d4d50cac
Shomukh Aldosari <shomukh.aldosari@gmail.com>
2026-06-17 22:10:32 +0000

    Fix Task 8 indentation

--- python-more_classes/8-rectangle.py
+++ python-more_classes/8-rectangle.py
@@ -70,7 +70,8 @@ class Rectangle:
         """Print a message when an instance is deleted."""
         Rectangle.number_of_instances -= 1
         print("Bye rectangle...")
-            @staticmethod
+
+    @staticmethod
     def bigger_or_equal(rect_1, rect_2):
         """Return the biggest rectangle based on area."""
         if not isinstance(rect_1, Rectangle):
