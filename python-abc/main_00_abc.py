952256e346cc3017acc16c33f076dff548bf20c3
Shomukh Al Dosari <shomukh.aldosari@gmail.com>
2026-06-21 23:14:04 +0000

    Task 0: Abstract Animal class

---
+++ python-abc/main_00_abc.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+from task_00_abc import Animal, Dog, Cat
+
+bobby = Dog()
+garfield = Cat()
+
+print(bobby.sound())
+print(garfield.sound())
+
+animal = Animal()
+print(animal.sound())
