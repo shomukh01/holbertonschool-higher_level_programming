4bba530f0e0c0537a8c8b40ada92a43e5500a857
Shomukh Al Dosari <shomukh.aldosari@gmail.com>
2026-06-21 23:05:58 +0000

    Task 8: Rectangle

---
+++ python-inheritance/8-rectangle.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Module that defines a Rectangle class."""
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """Rectangle class."""
+
+    def __init__(self, width, height):
+        """Initialize a rectangle."""
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
