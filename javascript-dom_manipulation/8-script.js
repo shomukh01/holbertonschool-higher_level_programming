b30782de4374bb3368edfd64d920e23e3f3b7cc0
shomukh01 <shomukh.aldosari2@gmail.com>
2026-08-16 03:15:14 +0200

    Add task 8 say hello

---
+++ javascript-dom_manipulation/8-script.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', () => {
+  fetch('https://hellosalut.stefanbohacek.com/?lang=fr')
+    .then((response) => response.json())
+    .then((data) => {
+      document.querySelector('#hello').textContent = data.hello;
+    });
+});
